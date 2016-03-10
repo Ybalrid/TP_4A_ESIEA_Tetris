7f680a47ad61866ed6f259014591ff57112583e2
Ybalrid <ybalrid@cecmu.org>
2016-03-10 10:49:41 +0000

    Main class create Game. Game init Grid.

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,7 +1,28 @@
 package game;
 
+import model.Grid;
+
 /**
  * Created by kuro on 10/03/16.
  */
 public class Game {
+
+    private Grid grid;
+
+    public Game()
+    {
+        grid = new Grid();
+    }
+
+    /**
+     * Launch the main loop of the game
+     */
+    public void loop()
+    {
+
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
 }
