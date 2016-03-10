f475055ba1490fd419f7bfe8ba6def252b9c6787
Ybalrid <ybalrid@cecmu.org>
2016-03-10 10:49:41 +0000

    Main class create Game. Game init Grid.

--- src/main/java/Tetris.java
+++ src/main/java/Tetris.java
@@ -1,10 +1,15 @@
 package main.java;
 
+import game.Game;
+
 public class Tetris
 {
+    static Game game;
     public static void main(String[] args)
     {
         System.out.println("Hello java!");
+        game = new Game();
+        game.loop();
     }
 
     public static int returnTwo()
