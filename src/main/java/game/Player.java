d4bdd7920f7ff077813aaf4fc2cc086085873d65
Tenma <tenma@nuolezio.org>
2016-03-10 10:49:17 +0100

    Created Player class

---
+++ src/main/java/game/Player.java
@@ -0,0 +1,21 @@
+package game;
+
+/**
+ * Created by kuro on 10/03/16.
+ */
+public class Player {
+
+    private int score;
+
+    public Player() {
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
