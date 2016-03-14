1499e92965981d96255953e31789fe0e2e925cbd
Tenma <tenma@nuolezio.org>
2016-03-14 16:30:41 +0100

    Drawing grid

--- src/main/java/view/GridPanel.java
+++ src/main/java/view/GridPanel.java
@@ -2,6 +2,7 @@ package view;
 
 import game.Game;
 import model.Grid;
+import model.Position;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,9 +13,10 @@ import java.awt.*;
 public class GridPanel extends JPanel {
 
     private Grid grid;
+    private final Color[] tetrominoColors = new Color[7];
 
     public GridPanel(Game game) {
-        //this.grid = game.getGrid();
+        this.grid = game.getGrid();
         this.add(new JButton("Hello"));
     }
 
@@ -23,7 +25,17 @@ public class GridPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        
+
+        int val;
+        for (int i = 0; i < this.grid.getWidth(); i++) {
+            for (int j = 0; j < this.grid.getHeight(); j++) {
+                val = this.grid.get(new Position(i, j));
+                //g2.setColor();
+                g2.drawRect(10 * i, 10 * j, 10, 10);
+            }
+
+        }
+
         g2.dispose();
     }
 }
