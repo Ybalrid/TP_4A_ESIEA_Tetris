381b7229716eb3c6fb75203feb365a447237c357
Tenma <tenma@nuolezio.org>
2016-03-15 00:42:43 +0100

    Draw grid and cells

--- src/main/java/view/GridPanel.java
+++ src/main/java/view/GridPanel.java
@@ -15,6 +15,7 @@ public class GridPanel extends JPanel {
 
     private Grid grid;
     private final static int CELL_SIZE = 20;
+    private final static Color GRID_COLOR = Color.DARK_GRAY;
     /** T: purple, O: yellow, I: cyan, L: orange, J: blue, S: green, Z: red */
     private final Color[] tetrominoColors = new Color[] {
     	new Color(150, 50, 150),
@@ -27,8 +28,9 @@ public class GridPanel extends JPanel {
 
     public GridPanel(Game game) {
         this.grid = game.getGrid();
-        this.setPreferredSize(new Dimension(CELL_SIZE * this.grid.getWidth(), CELL_SIZE * this.grid.getHeight()));
+        this.setPreferredSize(new Dimension(CELL_SIZE * this.grid.getWidth(), CELL_SIZE * (this.grid.getHeight() - 1)));
         //this.add(new JButton("Hello"));
+        this.grid.set(new Position(1,2), TetrominoType.I.ordinal());
     }
 
     @Override
@@ -37,14 +39,26 @@ public class GridPanel extends JPanel {
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        int val;
-        for (int i = 0; i < this.grid.getWidth(); i++) {
-            for (int j = 0; j < this.grid.getHeight(); j++) {
-                val = this.grid.get(new Position(i, j));
-                g2.setColor(this.tetrominoColors[val]);
-                g2.drawRect(CELL_SIZE * i, CELL_SIZE * j, CELL_SIZE, CELL_SIZE);
-            }
+        int height = this.grid.getHeight()-1, width = this.grid.getWidth()-1;
 
+        g2.setColor(GRID_COLOR);
+        for (int i = 0; i < width; i++) {
+            g2.drawLine(CELL_SIZE * i, 0, CELL_SIZE * i, height * CELL_SIZE);
+        }
+        for (int i = 0; i < height; i++) {
+            g2.drawLine(0, CELL_SIZE * i, width * CELL_SIZE, CELL_SIZE * i);
+        }
+
+        g2.setStroke(new BasicStroke(5));
+        int val;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                val = this.grid.get(new Position(i, j));
+                if (val > Grid.DEFAULT_GRID_VALUE) {
+                    g2.setColor(this.tetrominoColors[val]);
+                    g2.fillRect(CELL_SIZE * i + 1, CELL_SIZE * j + 1, CELL_SIZE - 2, CELL_SIZE - 2);
+                }
+            }
         }
 
         g2.dispose();
