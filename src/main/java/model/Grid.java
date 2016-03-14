74dd764bf82c42b04a060978dd002970f3003abc
Tenma <tenma@nuolezio.org>
2016-03-15 00:49:40 +0100

    Changed default cell value in grid

--- src/main/java/model/Grid.java
+++ src/main/java/model/Grid.java
@@ -1,14 +1,19 @@
 package model;
 
+import java.util.Arrays;
+
 /**
  * Created by kuro on 10/03/16.
  */
 public class Grid {
 
     private int[][] grid;
+    public final static int DEFAULT_GRID_VALUE = -1;
 
     public Grid() {
         this.grid = new int[10][21];
+        for (int[] column : grid)
+            Arrays.fill(column, DEFAULT_GRID_VALUE);
     }
 
     public int[][] get() {
