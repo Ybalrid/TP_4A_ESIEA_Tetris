dc184faa03e5c6c503e5f241234c36c33697a474
vincent <vclement@et.esiea.fr>
2016-04-03 00:24:34 +0200

    Add files for JUNIT tests

---
+++ src/test/java/game/GameTest.java
@@ -0,0 +1,27 @@
+package game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vincet on 03/04/16.
+ */
+public class GameTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void loop() throws Exception {
+
+    }
+
+    @Test
+    public void getGrid() throws Exception {
+
+    }
+
+}
