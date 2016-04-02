93f5a2aa1c7f481c355aa3b16283e498b4ca8d8e
Tenma <tenma@nuolezio.org>
2016-04-02 23:14:27 +0200

    Modified grid display

--- src/main/java/view/GridPanel.java
+++ src/main/java/view/GridPanel.java
@@ -14,10 +14,10 @@ import java.awt.*;
 public class GridPanel extends JPanel {
 
     private Grid grid;
-    private final static int CELL_SIZE = 20;
+    private final static int CELL_SIZE = 25;
     private final static Color GRID_COLOR = Color.DARK_GRAY;
     /** T: purple, O: yellow, I: cyan, L: orange, J: blue, S: green, Z: red */
-    private final Color[] tetrominoColors = new Color[] {
+    private final static Color[] TETROMINO_COLORS = new Color[] {
     	new Color(150, 50, 150),
         new Color(200, 200, 0),
         new Color(50, 180, 180),
@@ -29,8 +29,12 @@ public class GridPanel extends JPanel {
     public GridPanel(Game game) {
         this.grid = game.getGrid();
         this.setPreferredSize(new Dimension(CELL_SIZE * this.grid.getWidth(), CELL_SIZE * (this.grid.getHeight() - 1)));
-        //this.add(new JButton("Hello"));
+        this.grid.set(new Position(3,3), TetrominoType.Z.ordinal());
         this.grid.set(new Position(1,2), TetrominoType.I.ordinal());
+        this.grid.set(new Position(1,4), TetrominoType.L.ordinal());
+        this.grid.set(new Position(1,1), TetrominoType.O.ordinal());
+        this.grid.set(new Position(4,5), TetrominoType.T.ordinal());
+        this.grid.set(new Position(7,2), TetrominoType.S.ordinal());
     }
 
     @Override
@@ -39,15 +43,13 @@ public class GridPanel extends JPanel {
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        int height = this.grid.getHeight()-1, width = this.grid.getWidth()-1;
+        int height = this.grid.getHeight()-1, width = this.grid.getWidth();
 
         g2.setColor(GRID_COLOR);
-        for (int i = 0; i < width; i++) {
+        for (int i = 0; i < width; i++)
             g2.drawLine(CELL_SIZE * i, 0, CELL_SIZE * i, height * CELL_SIZE);
-        }
-        for (int i = 0; i < height; i++) {
+        for (int i = 0; i < height; i++)
             g2.drawLine(0, CELL_SIZE * i, width * CELL_SIZE, CELL_SIZE * i);
-        }
 
         g2.setStroke(new BasicStroke(5));
         int val;
@@ -55,8 +57,8 @@ public class GridPanel extends JPanel {
             for (int j = 0; j < height; j++) {
                 val = this.grid.get(new Position(i, j));
                 if (val > Grid.DEFAULT_GRID_VALUE) {
-                    g2.setColor(this.tetrominoColors[val]);
-                    g2.fillRect(CELL_SIZE * i + 1, CELL_SIZE * j + 1, CELL_SIZE - 2, CELL_SIZE - 2);
+                    g2.setColor(TETROMINO_COLORS[val]);
+                    g2.fillRect(CELL_SIZE * i + 1, CELL_SIZE * j + 1, CELL_SIZE - 1, CELL_SIZE - 1);
                 }
             }
         }
