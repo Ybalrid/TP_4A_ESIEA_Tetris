72f9f2a3cf43f3777d0338ef4530cdb6e5d66b68
vincent <vclement@et.esiea.fr>
2016-04-03 00:24:34 +0200

    Add files for JUNIT tests

---
+++ src/test/java/model/GridTest.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vincet on 03/04/16.
+ */
+public class GridTest {
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test
+    public void get1() throws Exception {
+
+    }
+
+    @Test
+    public void set() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/model/PositionTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vincet on 03/04/16.
+ */
+public class PositionTest {
+    @Test
+    public void getX() throws Exception {
+
+    }
+
+    @Test
+    public void setX() throws Exception {
+
+    }
+
+    @Test
+    public void getY() throws Exception {
+
+    }
+
+    @Test
+    public void setY() throws Exception {
+
+    }
+
+}
