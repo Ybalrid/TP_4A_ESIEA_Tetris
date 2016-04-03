dfecad2d346198a37576101d218ecdf015171081
vincent <vclement@et.esiea.fr>
2016-04-03 23:21:38 +0200

    Add tests for Grid class

--- src/test/java/model/GridTest.java
+++ src/test/java/model/GridTest.java
@@ -8,30 +8,58 @@ import static org.junit.Assert.*;
 /**
  * Created by vincet on 03/04/16.
  */
-public class GridTest {
+public class GridTest extends Grid{
+    public GridTest(){
+        super();
+    }
     private Grid objectTest;
+    Position pos = new Position(10,45);
     @Before
     public void setUp() throws Exception {
-        objectTest = new Grid();
+        objectTest = new GridTest();
     }
 
     @Test
-    public void get() throws Exception {
+    public void getTest() throws Exception {
+        int newgrid = objectTest.get(pos);
+        assertTrue("valeur correcte", objectTest.get(pos) == newgrid);
+    }
+
+    @Test
+    public void setTest() throws Exception {
+        objectTest.set(pos,98);
+        assertTrue("valeur mise a jour", objectTest.get(pos)==98);
+    }
+
+    @Test
+    public void applyTetrominoTest() throws Exception {
+
+    }
+
+
+    @Test
+    private void fall() {
 
     }
 
     @Test
-    public void set() throws Exception {
+    private int checkAndClearLines() {
 
     }
 
     @Test
-    public void applyTetromino() throws Exception {
+    private boolean isLineFull(int y) {
+        Position pos1 = new Position(10,5)
+        objectTest.set(pos,5);
+    }
+
+    @Test
+    private boolean isLineVoid(int y) {
 
     }
 
     @Test
-    public void update() throws Exception {
+    private void clearLine(int y) {
 
     }
 
